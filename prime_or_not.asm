98735b26d288243dfc72e054179432c7f671d136
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2022-03-16 23:49:36 +0530

    Add files via upload

---
+++ prime_or_not.asm
@@ -0,0 +1,50 @@
+section .data
+	msg1 db "The given number is prime!", 0xa, 0xd
+	len1 equ $ - msg1
+
+	msg2 db "The given number is not prime", 0xa, 0xd
+	len2 equ $ - msg2
+
+section .text
+	global _start
+
+_start:
+	mov ecx, 25      ; i stored the input number in ecx
+
+	; get esi (num/2)   ; for prime or not , it is enough to check till n/2
+	mov eax, ecx        ;
+	mov bx, 2
+	div bx
+	mov esi, eax        ;now esi is number/2
+
+div_loop:
+	mov eax, ecx
+	xor edx, edx
+	cmp esi, 1
+	je verdict_prime
+	mov ebx, esi
+	div ebx
+	dec esi
+	cmp edx, 0
+	jne div_loop
+
+verdict_not_prime:
+	mov eax, 4
+    	mov ebx, 1
+    	mov ecx, msg2
+   	mov edx, len2
+    	int 0x80
+    	jmp exit
+
+verdict_prime:
+    	mov eax, 4
+    	mov ebx, 1
+    	mov ecx, msg1
+    	mov edx, len1
+    	int 0x80
+    	jmp exit
+
+exit:
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
